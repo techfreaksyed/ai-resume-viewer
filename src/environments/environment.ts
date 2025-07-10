0e37522b05e394a6e678f252b8eb0ae69f107058
techfreaksyed <ukundertaker32@gmail.com>
2025-07-11 00:16:30 +0530

    removed environment files

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  geminiApiKey: 'AIzaSyAlJuUjIFTIc-QIt7lANlECvEFWXmUOQV4', // Replace with your actual key
-  appwriteEndpoint: 'https://fra.cloud.appwrite.io/v1',
-  appwriteProjectId: '686b7cad00104e2ddfad',
-  appwriteBucketId: '686c9946001b436d0bd5', // Replace with your actual bucket ID
-};
